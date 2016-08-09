05776ddf07bb250c9799a284b52b8067e866e7cd
iHosam <h.alhaythami@gmail.com>
2016-08-09 09:10:22 +0300

    Update L1Q2.sh

--- Labs/Lab1/Q2/L1Q2.sh
+++ Labs/Lab1/Q2/L1Q2.sh
@@ -6,15 +6,3 @@
 # the first argument ( $1 ) is the shape selection while the second argument ( $2 )
 # is the number of iteration.
 
-# This shell program will ask for two integers and do
-# sum,subtract,divide and multiply them
-# this shell program dont output floating numbers
-
-echo "Enter first integer"
-read num1
-echo "Enter second integer"
-read num2
-echo "summation =" $((num1+num2))
-echo "subtraction =" $((num1-num2))
-echo "multiplication =" $((num1*num2))
-echo "division =" $((num1/num2))
